3fe115341f1dfee91cfa78696243813296aec94f
Dmytro Tarasenko <dmytro.tarasenko.52@gmail.com>
2025-03-20 00:21:27 +0200

    completed project

--- server.js
+++ server.js
@@ -3,7 +3,16 @@ const path = require('path');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/dynamic', (req, res) => {
+    res.render('dynamic', { 
+        title: 'Dynamic Page', 
+        message: 'On the static page I said a few words about me and WEC. And here I will add photo of Formula 1.'
+    });
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
